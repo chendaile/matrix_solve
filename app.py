e322bd3cbb115e69442ec95daf21350dc50a81c9
oftheloneliness <3185854290@qq.com>
2025-02-28 12:21:58 +0800

    2.28

--- app.py
+++ app.py
@@ -108,4 +108,4 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000)  # 仅限本机访问
+    app.run(host='localhost', port=5000, debug=True)  # 添加 debug=True
